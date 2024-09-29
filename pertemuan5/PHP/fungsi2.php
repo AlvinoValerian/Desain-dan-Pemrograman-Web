3e6fec9bcee037bcaf7d83c1b86a5da54f33ea28
AlvinoValerian <alvino41127@gmail.com>
2024-09-29 23:10:04 +0700

    fungsiReturn

---
+++ pertemuan5/PHP/fungsi2.php
@@ -0,0 +1,15 @@
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+function perkenalan($nama, $salam="Assalamualikum") {
+    echo $salam. ",";
+    echo"Perkenalkan, nama saya ".$nama."</br>";
+
+    echo "saya berusia ".hitungUmur(2004,2024) . " tahun<br>";
+    echo "Senang berkenalan dengan anda</br>";
+}
+
+perkenalan("Elok");
+?>
