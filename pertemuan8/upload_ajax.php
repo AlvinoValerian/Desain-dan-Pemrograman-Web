dafb01a1b55b3315c8ad751e52652b2de3f716c3
AlvinoValerian <alvino41127@gmail.com>
2024-10-21 02:08:11 +0700

    praktikum4

--- pertemuan8/upload_ajax.php
+++ pertemuan8/upload_ajax.php
@@ -1,16 +1,16 @@
 <?php
-if(isset($_FILES['files'])){
+if(isset($_FILES['file'])){
     $errors = array();
-    $allowed_extensions = array("jpeg", "jpg", "png", "gif");
+    $allowed_extensions = array("jpeg", "jpg", "png", "gif" ,"doc","docx","pdf");
     
-    foreach($_FILES['files']['tmp_name'] as $key => $tmp_name) {
-        $file_name = $_FILES['files']['name'][$key];
-        $file_size = $_FILES['files']['size'][$key];
-        $file_tmp = $_FILES['files']['tmp_name'][$key];
+    foreach($_FILES['file']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['file']['name'][$key];
+        $file_size = $_FILES['file']['size'][$key];
+        $file_tmp = $_FILES['file']['tmp_name'][$key];
         $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
         if(!in_array($file_ext, $allowed_extensions)) {
-            $errors[] = "$file_name: Ekstensi file yang diizinkan adalah JPEG, JPG, PNG, atau GIF.";
+            $errors[] = "$file_name: Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, JPEG, JPG, PNG, atau GIF.";
         }
         if($file_size > 2097152) {
             $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
