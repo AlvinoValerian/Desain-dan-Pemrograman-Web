c06d78e020c61bde4db26255034585dc744aadc6
AlvinoValerian <alvino41127@gmail.com>
2024-10-20 22:42:10 +0700

    praktikum3

---
+++ pertemuan8/upload_ajax.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_FILES['files'])){
+    $errors = array();
+    $allowed_extensions = array("jpeg", "jpg", "png", "gif");
+    
+    foreach($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['files']['name'][$key];
+        $file_size = $_FILES['files']['size'][$key];
+        $file_tmp = $_FILES['files']['tmp_name'][$key];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+        if(!in_array($file_ext, $allowed_extensions)) {
+            $errors[] = "$file_name: Ekstensi file yang diizinkan adalah JPEG, JPG, PNG, atau GIF.";
+        }
+        if($file_size > 2097152) {
+            $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
+        }
+        if(empty($errors)) {
+            move_uploaded_file($file_tmp, "document/" . $file_name);
+            echo "$file_name berhasil diunggah.<br>";
+        }
+    }
+
+    if(!empty($errors)){
+        echo implode("<br>", $errors);
+    }
+}
+
+
+?>
+
